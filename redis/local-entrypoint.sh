c384ede7772847095d20fd2acdab77c0aca2e0a1
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-05-15 13:04:53 +0200

    Use confd in redis
    And so regenerate conf file on each start

--- redis/local-entrypoint.sh
+++ redis/local-entrypoint.sh
@@ -3,19 +3,20 @@ set -e
 
 giddyup service wait scale --timeout 120
 
-my_ip=$(giddyup ip myip)
+export MY_IP=$(giddyup ip myip)
 master_ip=$(giddyup leader get)
 
-echo "my_ip=$my_ip"
+echo "my_ip=$MY_IP"
 echo "master_ip=$master_ip"
-
-sed -i s/%my_ip%/$my_ip/g /usr/local/etc/redis/redis.conf
 
 { giddyup leader check \
   && echo "i am the leader"
 } || {
   echo "i am not the leader"
-  echo "slaveof $master_ip 6379" >> /usr/local/etc/redis/redis.conf
+  export SLAVEOF="slaveof $master_ip 6379"
 }
 
-docker-entrypoint.sh "$@"
+confd -onetime -backend env
+chown redis:redis /usr/local/etc/redis/redis.conf
+
+exec docker-entrypoint.sh "$@"
