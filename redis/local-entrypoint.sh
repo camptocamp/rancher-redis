cd99215be672f35c4f8e49dd5e7c40326e0674f7
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-07-13 17:59:35 +0200

    Search the current master when starting
    When a container is started, the master is sometimes no longer the
    rancher's primary ip, due to a failover that switched the master. A
    slave would try to connect on another slave. The startup script searches
    for the current master, and if it can't find any (first start), then, it
    chooses the primary ip.

--- redis/local-entrypoint.sh
+++ redis/local-entrypoint.sh
@@ -3,18 +3,39 @@ set -e
 
 giddyup service wait scale --timeout 120
 
+stack_name=$(curl -s http://rancher-metadata/2016-07-29/self/stack/name)
+container_name="redis"
+
+my_ip=$(giddyup ip myip)
+
+master_ip=''
+containers_ip=$(giddyup ip stringify --delimiter=" ")
+for container_ip in $containers_ip; do
+  if [ "${my_ip}" != "${container_ip}" ]; then
+    set +e
+    role=$(redis-cli -h $container_ip -p 6379 info | grep role | tr -d '\r')
+    set -e
+    if [ "${role}" = "role:master" ]; then
+      master_ip=$container_ip
+    fi
+  fi
+done
+
+if [ -z "${master_ip}" ]; then
+  master_ip=$(giddyup leader get)
+fi
+
 export MY_IP=$(giddyup ip myip)
-master_ip=$(giddyup leader get)
 
-echo "my_ip=$MY_IP"
-echo "master_ip=$master_ip"
-
-{ giddyup leader check \
-  && echo "i am the leader"
-} || {
-  echo "i am not the leader"
+if [ "${MY_IP}" = "${master_ip}" ]; then
+  echo "role: master"
+else
+  echo "role: slave"
   export SLAVEOF="slaveof $master_ip 6379"
-}
+fi
+
+echo "IP: $MY_IP"
+echo "Master IP: $master_ip"
 
 confd -onetime -backend env
 chown redis:redis /usr/local/etc/redis/redis.conf
--- sentinel/local-entrypoint.sh
+++ sentinel/local-entrypoint.sh
@@ -8,6 +8,8 @@ function leader_ip {
 giddyup service wait scale --timeout 120
 
 stack_name=$(curl -s http://rancher-metadata/2016-07-29/self/stack/name)
+container_name="redis"
+
 export MY_IP=$(giddyup ip myip)
 
 while [ "$(leader_ip $stack_name "redis")" = "Not found" ]
@@ -15,7 +17,26 @@ do
   echo "Waiting for redis to start up"
   sleep 0.1
 done
-export MASTER_IP=$(leader_ip $stack_name "redis")
+
+master=''
+containers=$(curl -s http://rancher-metadata/2016-07-29/stacks/${stack_name}/services/${container_name}/containers)
+for container_output in $containers; do
+  # container_output is like 0=redis-sentinel-redis-1
+  container_idx=$(echo $container_output | sed 's/=.*//')
+  container_ip=$(curl -s http://rancher-metadata/2016-07-29/stacks/${stack_name}/services/${container_name}/containers/${container_idx}/primary_ip)
+  set +e
+  role=$(redis-cli -h $container_ip -p 6379 info | grep role | tr -d '\r')
+  set -e
+  if [ "${role}" = "role:master" ]; then
+    master=$container_ip
+  fi
+done
+
+if [ -z "${master}" ]; then
+  export MASTER_IP=$master
+else
+  export MASTER_IP=$(leader_ip $stack_name "redis")
+fi
 
 echo "stack_name=$stack_name"
 echo "my_ip=$MY_IP"
